1a3a642863d953512ac9c9a5c79b835e7ed327ed
Pirun Seng <sengpirun.rain@gmail.com>
2020-12-10 09:19:50 +0700

    section 24 video 342 merging list of records and handling CRUD

---
+++ streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,31 @@
+import _ from "lodash";
+import {
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "../actions/types";
+
+const streamReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      // ..._.mapKeys(action.payload), extract object from mapKeys and merged into the same outer obj
+      return { ...state, ..._.mapKeys(action.payload) };
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      // delete payload is the id
+      // return { ...state, [action.payload]: undefined };
+      return _.omit(state, action.payload);
+
+    default:
+      return state;
+  }
+};
+
+export default streamReducer;
